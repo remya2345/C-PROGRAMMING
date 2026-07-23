5f52d9e0432970eb7ac9ff43c8579d96a6a703d8
Remya S <remya2544@gmail.com>
2026-07-23 18:59:46 +0530

    Update program2.c

--- program2.c
+++ program2.c
@@ -4,13 +4,13 @@
 void main()
 {
 	int a,b,c;
-	 printf("enter the 3 numbers");
+	 printf("enter the 3 numbers :");
 	 scanf("%d%d%d",&a,&b,&c);
 	 if(a<b&&a<c)
-	  printf("%d is the smallest",a);
+	  printf("%d is the smallest :",a);
 	 else if(b<a&&b<c)
-	  printf("%d is the smallest",b);
+	  printf("%d is the smallest :",b);
 	 else
-	  printf("%d is the smallest",c);
+	  printf("%d is the smallest :",c);
 	 
 }
