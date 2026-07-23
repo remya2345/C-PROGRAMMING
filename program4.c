fab90a302aca6a3257cba9035aa233549d69c0e5
Remya S <remya2544@gmail.com>
2026-07-23 18:46:53 +0530

    Update program4.c

--- program4.c
+++ program4.c
@@ -1,11 +1,11 @@
-//PROGRAM TO CONVERT FARENHIT TO CELICIUS
+//PROGRAM TO CONVERT FAHRENHEIT TO CELSIUS
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
 	float f,c;
-	 printf(" Enter The Temp in Farenhit");
+	 printf(" Enter The Temp in Fahrenheit");
 	 scanf("%d",&f);
 	 c=(f-32) / 1.8;
-	 printf("celicius= %d",c);
+	 printf("celsius= %d",c);
 }
