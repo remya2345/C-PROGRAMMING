411c549da56cde2375c954d350600f26c4c470fb
Remya S <remya2544@gmail.com>
2026-08-05 12:15:30 +0530

    Add files via upload

---
+++ logical AND operator.c
@@ -0,0 +1,17 @@
+//logical operator AND
+#include <stdio.h> 
+int main() 
+{  
+int a = 10, b = 20; 
+if (a > 0 && b > 0) 
+{ 
+   printf("Both values are greater than 0\n");
+}  
+else 
+{ 
+   printf("One or both values are less than or equal to 0\n"); 
+    
+} 
+return 0;
+
+}
