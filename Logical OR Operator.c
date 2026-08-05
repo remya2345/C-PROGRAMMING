5af778e4df54e662ef677970e3e0583937b40026
Remya S <remya2544@gmail.com>
2026-08-05 12:11:16 +0530

    Add files via upload

---
+++ Logical OR Operator.c
@@ -0,0 +1,16 @@
+// Logical OR Operator ( || )
+#include <stdio.h> 
+int main() 
+{ 
+int a = -1, b = 20; 
+if (a > 0 || b > 0) 
+{ 
+printf("Any one of the given value is " 
+"greater than 0\n");
+}
+else 
+{ 
+printf("Both values are less than or equal to 0\n"); 
+}
+return 0; 
+}
