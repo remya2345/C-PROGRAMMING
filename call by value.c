dfc2be1cb41caef4c768f266c28de9550b79884c
Remya S <remya2544@gmail.com>
2026-09-15 19:50:52 +0530

    Add files via upload

---
+++ call by value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void change(int x)
+{
+	x=10;
+}
+int main()
+{
+	int a=20;
+	printf("before function call=%d\n",a);
+	change(a);
+	printf("after function call=%d\n",a);
+	
+}
+
