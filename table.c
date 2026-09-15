f28ba0d821d65e60102ee490aff1163f1b2446b9
Remya S <remya2544@gmail.com>
2026-09-15 19:02:16 +0530

    Update multiplicationtable.c

--- table.c
+++ table.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 #include<conio.h>
-void table()
+void mul()
 {
-	
-	int i;
-	for(i=1;i<=10;i++)
+	int i,j,n;
+	printf("enter a number");
+	scanf("%d",&n);
+	for(i=1;i<10;i++)
 	{
-		printf("5 x %d=%d\n",i,5 * i);
+		printf("%d * %d=%d\n",n,i,n*i);
 	}
+	
 }
 int main()
 {
-	table();
+	mul();
 }
+
 
